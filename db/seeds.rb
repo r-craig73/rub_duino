6c35bac1077ce085c21a9bebdcfe051fec2085fd
Ron Craig <ron.craig@comcast.net>
2018-05-15 13:01:23 -0700

    edit seed pin number to 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Sensor.destroy_all
 sensor_names =  ["Blue LED", "Green LED", "Yellow LED"]
 sensor_descriptions = ["A simple Blue LED", "A common Green LED", "A normal Yellow LED"]
 sensor_kinds = "Digital"
-sensor_pins = 99
+sensor_pins = 0
 sensor_boards = "board"
 
 sensor_names.length.times do |i|
