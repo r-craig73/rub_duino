6b6366d55e33e76c6fd5aad3616a9ef198220cc5
Ron Craig <ron.craig@comcast.net>
2018-05-15 15:57:44 -0700

    add blinking methods and comment out module LcdBlink

--- app/controllers/concerns/lcd_blink.rb
+++ app/controllers/concerns/lcd_blink.rb
@@ -1,10 +1,38 @@
-module LcdBlink
+# module LcdBlink
 require 'dino'
 board = Dino::Board.new(Dino::TxRx.new)
-led = Dino::Components::Led.new(pin: 13, board: board)
-  [:on, :off].cycle do |switch|
-    led.send(switch)
-    puts switch
-    sleep 2.5
+@red_led = Dino::Components::Led.new(pin: 13, board: board)
+
+  def forever_wink(led)
+    [:on, :off].cycle do |switch|
+      led.send(switch)
+      puts switch
+      sleep 0.5
+    end
   end
-end
+
+  def slow_wink (led)
+    led.send(:on)
+    puts :on
+    sleep 2
+    led.send(:off)
+    puts :off
+    sleep 2
+  end
+
+  def wink(led)
+    led.send(:on)
+    sleep 0.2
+    led.send(:off)
+    sleep 0.2
+  end
+
+  10.times do
+    wink(@red_led)
+  end
+
+  3.times do
+    slow_wink(@red_led)
+  end
+
+# end
