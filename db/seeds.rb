530fb2fa34b662ee8798c83eac736994d529f325
Ron Craig <ron.craig@comcast.net>
2018-05-15 11:46:24 -0700

    edit seed to add 3 fake sensors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Sensor.destroy_all
-Measure.destroy_all
+# Measure.destroy_all
 
 sensor_names =  ["Blue LED", "Green LED", "Yellow LED"]
 sensor_descriptions = ["A simple Blue LED", "A common Green LED", "A normal Yellow LED"]
@@ -7,9 +7,9 @@ sensor_kinds = "Digital"
 sensor_pins = 99
 sensor_boards = "board"
 
-skill_names.length.times do |i|
-   Sensor.create!(name: sensor_names[i], kind: sensor_kinds, description: sensor_descriptions[i], pin: sensor_pins, board: sensor_boards)
-   @skill_id = Skill.last.id
- end
+sensor_names.length.times do |i|
+ Sensor.create!(name: sensor_names[i], kind: sensor_kinds, description: sensor_descriptions[i], pin: sensor_pins, board: sensor_boards)
+ @sensor_id = Sensor.last.id
+end
 
- p "Created #{Sensor.count} make believe sensors"
+p "Created #{Sensor.count} make believe sensors"
