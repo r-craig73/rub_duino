6af489bf16fa984b3b062c022969ef3a4c667bee
Ron Craig <ron.craig@comcast.net>
2018-05-15 09:07:03 -0700

    add sensors routing, starting sensors CRUD-L

---
+++ app/controllers/sensors_controller.rb
@@ -0,0 +1,40 @@
+class SensorsController < ApplicationController
+  before_filter :set_up_led, only: [:on, :off]
+
+  def index
+    @sensors = Sensor.all
+  end
+
+  def new
+    @sensor = Sensor.new
+  end
+
+  def create
+   @sensor = sensor.new(sensor_params)
+   if @sensor.save
+     flash[:notice] = "Your sensor is added to the list"
+     redirect_to sensors_path
+   else
+     render :new
+   end
+ end
+
+  def destroy
+    @sensor = Sensor.find(params[:id])
+    @sensor.destroy
+    flash[:notice] = "The sensor is removed"
+  end
+
+
+
+private
+
+  def set_up_led
+    @sensor = Dino::Components::Led.new(pin: 13, board: board)
+  end
+
+  def sensor_params
+     params.require(:sensor).permit(:name)
+  end
+
+end
