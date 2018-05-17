ba2ca9b24fedb5ac5a6157be3b0104b9157da539
Ron Craig <ron.craig@comcast.net>
2018-05-17 10:15:03 -0700

    edit show.html.erb file and delete migration files

--- db/migrate/20180515162949_delete_kind_column_from_sensors_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteKindColumnFromSensorsTable < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :sensors, :kind
-  end
-end
--- db/migrate/20180515181759_add_kind_into_sensors_table.rb
+++
@@ -1,5 +0,0 @@
-class AddKindIntoSensorsTable < ActiveRecord::Migration[5.2]
-  def change
-    add_column :sensors, :kind, :string
-  end
-end
