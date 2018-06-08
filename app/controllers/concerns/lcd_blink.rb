3f49936a835af63115fc15b41c69e96924b04461
Ron Craig <ron.craig@comcast.net>
2018-06-08 16:29:29 -0700

    add function to run dino known colors, cannot control actual color

--- app/controllers/concerns/lcd_blink.rb
+++ app/controllers/concerns/lcd_blink.rb
@@ -1,35 +1,68 @@
+require 'bundler/setup'
 require 'dino'
+
 board = Dino::Board.new(Dino::TxRx.new)
-@red_led = Dino::Components::Led.new(pin: 11, board: board)
+@one_led = Dino::Components::Led.new(pin: 11, board: board)
+@rgb_led = Dino::Components::RgbLed.new(pins: {red: 3, green: 5, blue: 6}, board: board)
 
 def forever_wink(led)
   [:on, :off].cycle do |switch|
     led.send(switch)
-    puts switch
+    p switch
     sleep 0.5
   end
 end
 
 def slow_wink(led)
-  led.send(:on)
-  puts :on
+  led.on
+  p :on
   sleep 2
-  led.send(:off)
-  puts :off
+  led.off
+  p :off
   sleep 2
 end
 
 def wink(led)
-  led.send(:on)
+  led.on
   sleep 0.2
-  led.send(:off)
+  led.off
   sleep 0.2
 end
 
-10.times do
-  wink(@red_led)
+def rgb_twocolor(led)
+  [:red, :green, :blue, :cyan, :yellow, :magenta, :white, :off].each do |switch|
+    p switch
+    led.send(switch)
+    sleep 1
+  end
 end
 
-3.times do
-  slow_wink(@red_led)
+def rgb_color(led)
+
+  # colors to consider rainbow ROYGBIV
+  # color(255, 0, 0) red
+  # color(255, 149, 7) orange
+  # color(255, 255, 0) yellow
+  # color(0, 255, 0) green
+  # color(0, 0, 255) blue
+  # color(75, 0, 130) indigo
+  # color(148, 0, 211) violet
 end
+
+
+
+# 10.times do
+#   wink(@one_led)
+# end
+#
+# 3.times do
+#   slow_wink(@one_led)
+# end
+
+#@rgb_led.color(0, 0, 255)
+
+
+
+# 2.times do
+#   rgb_twocolor(@rgb_led)
+# end
