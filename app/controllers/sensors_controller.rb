a14bd736a20f5eed6a069fa404c5bf408a68bc9d
Ron Craig <ron.craig@comcast.net>
2018-07-24 17:43:36 -0700

    editing and removing comment line

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class SensorsController < ApplicationController
-  # before_filter :set_up_led, only: [:on, :off]
-  before_action :set_up_led, only: [:on, :off]
+  before_action :set_up_led, only: %i[on off]
 
   def index
     @sensors = Sensor.all
@@ -17,12 +18,12 @@ class SensorsController < ApplicationController
 
   def update
     @sensor = Sensor.find(params[:id])
-      if @sensor.update(sensor_params)
-        flash[:notice] = "Sensor is now updated"
-        redirect_to sensors_path
-      else
-        render :edit
-      end
+    if @sensor.update(sensor_params)
+      flash[:notice] = 'Sensor is now updated'
+      redirect_to sensors_path
+    else
+      render :edit
+    end
   end
 
   def new
@@ -30,30 +31,29 @@ class SensorsController < ApplicationController
   end
 
   def create
-   @sensor = Sensor.new(sensor_params)
-   if @sensor.save
-     redirect_to sensors_path
-     flash[:notice] = "The sensor is added to the Sensors Table"
-   else
-     render :new
-   end
- end
+    @sensor = Sensor.new(sensor_params)
+    if @sensor.save
+      redirect_to sensors_path
+      flash[:notice] = 'Sensor is added to the Sensors Table'
+    else
+      render :new
+    end
+  end
 
   def destroy
     @sensor = Sensor.find(params[:id])
     @sensor.destroy
     redirect_to sensors_path
-    flash[:notice] = "The sensor is removed from the Sensors Table"
+    flash[:notice] = 'Sensor is removed from the Sensors Table'
   end
 
-private
+  private
 
   def set_up_led
-    @led = Dino::Components::Led.new(pin: 13, board: board)
+    @led = Dino::Components::Led.new(pin: 9, board: board)
   end
 
   def sensor_params
-     params.require(:sensor).permit(:name, :description, :pin, :kind)
+    params.require(:sensor).permit(:name, :description, :pin, :kind)
   end
-
 end
