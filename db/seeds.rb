12bfa443474ec5f33b434f259dcc47f514c6fb70
Ron Craig <ron.craig@comcast.net>
2018-07-25 13:32:59 -0700

    edit seeds to include LED and fake sensors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 Sensor.destroy_all
 # Measure.destroy_all
 
-sensor_names =  ["Blue LED", "Green LED", "Yellow LED"]
-sensor_descriptions = ["A simple Blue LED", "A common Green LED", "A normal Yellow LED"]
-sensor_kinds = "Digital"
-sensor_pins = 0
-sensor_boards = "board"
+sensor_names = ['Red LED', 'RGB LED', 'Blue LED', 'Large Fan']
+sensor_details = ['A simple Red LED', 'A tri-color RGB LED', 'A single Blue LED', 'A motor with a fan that can blow away cats, dog and people']
+sensor_kinds = ['Digital', 'Digital', 'Digital', 'Analog']
+sensor_pins = [9, 3, 11, 2]
+sensor_boards = 'board'
 
 sensor_names.length.times do |i|
- Sensor.create!(name: sensor_names[i], kind: sensor_kinds, description: sensor_descriptions[i], pin: sensor_pins, board: sensor_boards)
- @sensor_id = Sensor.last.id
+  Sensor.create!(name: sensor_names[i], kind: sensor_kinds[i], description: sensor_details[i], pin: sensor_pins[i], board: sensor_boards)
+  @sensor_id = Sensor.last.id
 end
 
 p "Created #{Sensor.count} make believe sensors"
